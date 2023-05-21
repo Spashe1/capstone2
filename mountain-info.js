f7e74e70d08ecac0fe64cae973e52bff7c28de35
Sephora Pashe <Spashe@appdev.yearup.org>
2023-05-20 20:29:38 -0400

    page layout

---
+++ mountain-info.js
@@ -0,0 +1,6 @@
+"use strict"
+console.log(mountainsArray);
+mountainsArray.forEach(mountainsArray => {
+    console.log(mountainsArray.name);
+    console.log(mountainsArray.desc);
+})
---
+++ national_parks.js
@@ -0,0 +1,7 @@
+"use strict"
+console.log(nationalParksArray);
+
+nationalParksArray.forEach(nationalParksArray=>{
+    console.log(nationalParksArray.Location);
+    console.log(nationalParksArray.City);
+})
