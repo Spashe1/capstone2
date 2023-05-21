d74eccc86e5db32ad8be7d1afc034c011ce2668a
Sephora Pashe <Spashe@appdev.yearup.org>
2023-05-20 21:06:05 -0400

    script file 1

--- national_parks.js
+++ national_parks.js
@@ -1,7 +1,18 @@
 "use strict"
-console.log(nationalParksArray);
+//console.log(nationalParksArray);
+//check if the array is accesable
+//smoketest
+// nationalParksArray.forEach(nationalParksArray=>{
+//     console.log(nationalParksArray.Location);
+//     console.log(nationalParksArray.City);
+// });
 
-nationalParksArray.forEach(nationalParksArray=>{
-    console.log(nationalParksArray.Location);
-    console.log(nationalParksArray.City);
-})
+const locationSelector = document.getElementById('locationSelector');
+console.log(locationSelector)
+// create options and append it to the location selector
+
+nationalParksArray.forEach((location)=> {
+    const option = new Option(location.LocationName, location.LocationID);
+   locationSelector.appendChild(option);
+   //console.log(option)//smoketest
+});
