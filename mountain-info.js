47a44e379122fd94a0faeebf61b8c725d3781748
Sephora Pashe <Spashe@appdev.yearup.org>
2023-05-23 16:06:16 -0400

    scripts

--- mountain-info.js
+++ mountain-info.js
@@ -1,6 +1,52 @@
 "use strict"
-console.log(mountainsArray);
-mountainsArray.forEach(mountainsArray => {
-    console.log(mountainsArray.name);
-    console.log(mountainsArray.desc);
+//smoketest
+console.log('hey')
+// console.log(mountainsArray);
+// mountainsArray.forEach(mountainsArray => {
+//     console.log(mountainsArray.name);
+//     console.log(mountainsArray.desc);
+// })
+const mountainSelector = document.getElementById('mountainSelector');
+const mountainInfoContainer = document.getElementById('mountainInfoContainer');
+
+//load DD with mountain options
+mountainsArray.forEach((mountain)=> {
+    const option = document.createElement('option')
+    option.value = mountain.name;
+    option.textContent = mountain.name;
+    mountainSelector.appendChild(option);
+    //console.log(option)
+});
+
+//eventlistener 
+mountainSelector.addEventListener('change', function(){
+    const selectedMountain = mountainSelector.value;
+
+    console.log(selectedMountain)
+    //find selected mount in the array
+    const mountain= mountainsArray.find((m) => m.name === selectedMountain);
+    if (mountain){
+        mountainInfoContainer.innerHTML = '';
+
+    }
+    const mountainName = document.createElement('h3');
+    mountainName.textContent = mountain.name;
+
+    const description = document.createElement('p');
+    description.textContent = mountain.desc;
+
+    const elevation = document.createElement('p');
+    elevation.textContent = 'Elevation: ' + mountain.elevation + ' feet';
+
+    //add pics
+    const image = document.createElement('img');
+    image.src = "images/" + mountain.img;
+    image.alt = mountain.name;
+    console.log(image)
+    // Append the elements to the mountain info container
+    mountainInfoContainer.appendChild(mountainName);
+    mountainInfoContainer.appendChild(description);
+    mountainInfoContainer.appendChild(elevation);
+    mountainInfoContainer.appendChild(image);
 })
+
--- national_parks.js
+++ national_parks.js
@@ -1,4 +1,5 @@
 "use strict"
+console.log('hey hey');
 //console.log(nationalParksArray);
 //check if the array is accesable
 //smoketest
@@ -6,13 +7,266 @@
 //     console.log(nationalParksArray.Location);
 //     console.log(nationalParksArray.City);
 // });
+// get radio buttons by their ID
+const locationBtn = document.getElementById('locationBtn');
+const parkTypeBtn = document.getElementById('parkTypeBtn');
+const resetBtn = document.getElementById('resetBtn')
+ //smoketest
+//  locationbtn.addEventListener('click', function(){
+//     console.log('clicked')
+//  });
+//  parkTypeBtn.addEventListener('change', function(){
+//     console.log('changed')
+//  })
+const locationDropdownContainer = document.getElementById('locationDropdownContainer');
+const parkTypeDropdownContainer = document.getElementById('parkTypeDropdownContainer');
+const locationDropdown = document.getElementById('locationDropdown');
+const parkTypeDropdown = document.getElementById('parkTypeDropdown');
+const searchResultsContainer = document.getElementById('searchResultsContainer');
+console.log(searchResultsContainer)
+//smoketest
+// console.log(locationDropdownContainer);
+// console.log(parkTypeDropdownContainer);
+// console.log(locationDropdown);
+// console.log(parkTypeDropdown);
 
-const locationSelector = document.getElementById('locationSelector');
-console.log(locationSelector)
-// create options and append it to the location selector
+//add event listeners to the radbtns
 
-nationalParksArray.forEach((location)=> {
-    const option = new Option(location.LocationName, location.LocationID);
-   locationSelector.appendChild(option);
-   //console.log(option)//smoketest
+locationBtn.addEventListener('change', function() {
+    if (locationBtn.checked){
+       console.log('Location radio button checked');
+        //show/hide dropdowns
+        locationDropdownContainer.style.display = 'block';
+        parkTypeDropdownContainer.style.display = 'none';
+       
+    }
+});
+
+parkTypeBtn.addEventListener('change', function(){
+    if(parkTypeBtn.checked){
+     console.log('Park Type radio button  checked');
+        //show park type DD and hide location DD
+        locationDropdownContainer.style.display = 'none';
+        parkTypeDropdownContainer.style.display = 'block';
+    }
+});
+
+//location dropdown- function to pupulate the location DD
+function populateLocationDropdown(){
+    locationDropdown.innerHTML = '';
+
+    //add default option/ create options
+
+const defaultOption = document.createElement('option');
+defaultOption.value ='';
+defaultOption.textContent = 'Select a location';
+locationDropdown.appendChild(defaultOption);
+console.log(defaultOption)
+//add options from the locationData array.
+
+for(let i =  0; i < locationsArray.length; i++){
+    const option = document.createElement('option');
+    option.value = locationsArray[i];
+    option.textContent = locationsArray[i];
+    locationDropdown.appendChild(option);
+   // console.log(locationsArray[i])
+}
+}
+ populateLocationDropdown()
+
+ // event listener for location DD
+locationDropdown.addEventListener('change', function() {
+    const selectedLocation = locationDropdown.value;
+    const filteredParks = nationalParksArray.filter(park => park.State.toLowerCase() === selectedLocation.toLowerCase());
+    displaySearchResults(filteredParks);
+  });
+  
+// // //smoketest
+console.log(locationDropdown);
+console.log(locationDropdown.options);
+// //populate park type DD
+function populateParkTypeDropdown(){
+    //clear existing options
+    parkTypeDropdown.innerText = '';
+
+    // add default options
+    const defaultOption = document.createElement('option');
+    defaultOption.value = '';
+    defaultOption.textContent = 'Select a park type';
+    parkTypeDropdown.appendChild(defaultOption);
+  
+    // Add options from the parkTypesArray
+    for (let i = 0; i < parkTypesArray.length; i++) {
+      const option = document.createElement('option');
+      option.value = parkTypesArray[i];
+      option.textContent = parkTypesArray[i];
+      parkTypeDropdown.appendChild(option);
+    }
+    console.log(parkTypesArray)
+  }
+//   
+  populateParkTypeDropdown();//call the functions to populate the DDs
+ 
+
+// function to display search results
+function displaySearchResults(parks) {
+    // clear previous search results
+    searchResultsContainer.innerHTML = '';
+  
+//    // Display detailed information for each park
+//    parks.forEach((park) => {
+//     const parkInfo = document.createElement('div');
+
+//     // Create elements for park information
+//     const locationName = document.createElement('h3');
+//     locationName.textContent = park.LocationName;
+//     parkInfo.appendChild(locationName);
+
+//     const address = document.createElement('p');
+//     address.textContent = `Address: ${park.Address}`;
+//     parkInfo.appendChild(address);
+
+//     const city = document.createElement('p');
+//     city.textContent = `City: ${park.City}`;
+//     parkInfo.appendChild(city);
+
+//     const state = document.createElement('p');
+//     state.textContent = `State: ${park.State}`;
+//     parkInfo.appendChild(state);
+
+//     const zipCode = document.createElement('p');
+//     zipCode.textContent = `Zip: ${park.ZipCode}`;
+//     parkInfo.appendChild(zipCode);
+
+//     const phone = document.createElement('p');
+//     phone.textContent = `Phone: ${park.Phone}`;
+//     parkInfo.appendChild(phone);
+
+//     // Add park information to the search results container
+//     searchResultsContainer.appendChild(parkInfo);
+//   });
+  //table
+  const table = document.createElement('table');
+
+  //  table header
+  const headerRow = document.createElement('tr');
+  const headers = ['Location Name', 'Address', 'City', 'State', 'Zip', 'Phone'];
+  headers.forEach((headerText) => {
+    const header = document.createElement('th');
+    header.textContent = headerText;
+    headerRow.appendChild(header);
+  });
+  table.appendChild(headerRow);
+
+  //  table rows for each park
+  parks.forEach((park) => {
+    const parkRow = document.createElement('tr');
+
+    // table cells for each park property
+    const locationNameCell = document.createElement('td');
+    locationNameCell.textContent = park.LocationName;
+    parkRow.appendChild(locationNameCell);
+
+    const addressCell = document.createElement('td');
+    addressCell.textContent = park.Address;
+    parkRow.appendChild(addressCell);
+
+    const cityCell = document.createElement('td');
+    cityCell.textContent = park.City;
+    parkRow.appendChild(cityCell);
+
+    const stateCell = document.createElement('td');
+    stateCell.textContent = park.State;
+    parkRow.appendChild(stateCell);
+
+    const zipCodeCell = document.createElement('td');
+    zipCodeCell.textContent = park.ZipCode;
+    parkRow.appendChild(zipCodeCell);
+
+    const phoneCell = document.createElement('td');
+    phoneCell.textContent = park.Phone;
+    parkRow.appendChild(phoneCell);
+
+    // add the row to the table
+    table.appendChild(parkRow);
+  });
+
+  // add the table to the search results container
+  searchResultsContainer.appendChild(table);
+}
+// function to display park information based on selected park type
+function displayParkInfo(selectedParkType) {
+    // clear previous park information
+    searchResultsContainer.innerHTML = '';
+  
+    // Filter parks based on selected park type
+    //.includes(selectedParkType.toLowerCase()) checks if the lowercase 
+    //selectedParkType value is included in the lowercase LocationName of the park. 
+    //The includes()method returns true if the substring is found, and false otherwise.
+    const filteredParks = nationalParksArray.filter(park => park.LocationName.toLowerCase().includes(selectedParkType.toLowerCase()));
+  
+    // Check if any parks match the selected park type
+    if (filteredParks.length === 0) {
+      const message = document.createElement('p');
+      message.textContent = 'No parks found for the selected park type.';
+      searchResultsContainer.appendChild(message);
+    } else {
+      // Display park information for each matching park
+      filteredParks.forEach(park => {
+        const parkInfo = document.createElement('div');
+        
+        // Create elements for location information
+        const locationName = document.createElement('h3');
+        locationName.textContent = park.LocationName;
+        parkInfo.appendChild(locationName);
+  
+        const address = document.createElement('p');
+        address.textContent = `Address: ${park.Address}`;
+        parkInfo.appendChild(address);
+  
+        const city = document.createElement('p');
+        city.textContent = `City: ${park.City}`;
+        parkInfo.appendChild(city);
+  
+        const state = document.createElement('p');
+        state.textContent = `State: ${park.State}`;
+        parkInfo.appendChild(state);
+  
+        const zipCode = document.createElement('p');
+        zipCode.textContent = `Zip Code: ${park.ZipCode}`;
+        parkInfo.appendChild(zipCode);
+  
+        const phone = document.createElement('p');
+        phone.textContent = `Phone: ${park.Phone}`;
+        parkInfo.appendChild(phone);
+  
+        // Add park information to the search results container
+        searchResultsContainer.appendChild(parkInfo);
+       // console.log(parkInfo)
+      });
+    }
+  }
+  
+
+// Event listener for park type dropdown
+parkTypeDropdown.addEventListener('change', function() {
+  const selectedParkType = parkTypeDropdown.value;
+  displayParkInfo(selectedParkType);
+  console.log(selectedParkType)
+});
+resetBtn.addEventListener('click', function() {
+  // Clear the search results container
+  searchResultsContainer.innerHTML = '';
+  
+  // Reset the radio buttons
+  locationBtn.checked = false;
+  parkTypeBtn.checked = false;
+  
+  // Reset the dropdowns
+  locationDropdown.selectedIndex = 0;
+  parkTypeDropdown.selectedIndex = 0;
+  
+  // Hide the dropdown containers
+  locationDropdownContainer.style.display = 'none';
+  parkTypeDropdownContainer.style.display = 'none';
 });
